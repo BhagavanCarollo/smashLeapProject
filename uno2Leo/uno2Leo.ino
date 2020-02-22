3979831c4b809a18d9bb4d7ef6f9a4c73da25caa
StephenSchuster <sschuster2021@francisparker.org>
2020-02-21 16:46:05 -0800

    updated again

--- uno2Leo/uno2Leo.ino
+++ uno2Leo/uno2Leo.ino
@@ -15,12 +15,8 @@ void loop()
    if ( Serial.available()) //recive value from proccesing
   {  // If data is available,
   val = Serial.read();         // read it and store it in val
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);
   }
-  x = ((int)val);
+  x = (byte)((int)val);
  Serial.println(val);
 Wire.beginTransmission(4); // transmit to device #4
 //Wire.write("x is "); 
